980ec1cc702f24d49fb916d0d0b6f35e6c4cb8e7
joao_arthurc <joao.arthur@nre.tec.br>
2022-06-28 00:02:59 -0300

    new Features

--- src/roteador/index.ts
+++ src/roteador/index.ts
@@ -1,6 +1,8 @@
 import { createRouter, createWebHashHistory, RouteRecordRaw } from "vue-router"
 import Tarefas from '../views/Tarefas.vue'
 import Projetos from '../views/Projetos.vue'
+import FormularioProjeto from '../views/Projetos/Formulario.vue'
+import ListaProjetos from '../views/Projetos/Lista.vue';
 
 const rotas: RouteRecordRaw[] = [
   {
@@ -9,11 +11,27 @@ const rotas: RouteRecordRaw[] = [
     component: Tarefas
   },
   {
-    path:'/projetos',
-    name: 'Projetos',
-    component: Projetos
-  },
-
+    path: '/ProjeTos',
+    component: Projetos,
+    children: [
+      {
+        path:'',
+        name: 'ListaProjetos',
+        component: ListaProjetos
+      },
+      {
+        path:'novo',
+        name: 'Novo Projeto',
+        component: FormularioProjeto
+      },
+      {
+        path:':id',
+        name: 'Editar Projeto',
+        component: FormularioProjeto,
+        props: true
+      },
+    ]
+  }
 ];
 
 const roteador = createRouter({
